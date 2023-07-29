88f1f93be0289b3e3614459914d6257f851abbdc
Vítor Maciel Gonçalves <vitormg93@gmail.com>
2023-07-29 16:33:15 -0300

    Update 07/29/2023
    - Merged the two functions into one to reduce unnecessary complexity;
    - Changed comments and variable/function names to adhere to the "Clean Code" standards;
    - Added check_user_input function to check if an integer was typed;
    - Added line skips for clarity;
    - Rebuilt the executable with the aforementioned changes.

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Why not say hello triangularly?
 This is a simple Python program with an included executable to play around with saying hello (triangularly).
 
 Its functions are made using the concept of recursion and it was a fun project that I decided to make after the all-so-boring traditional "Hello World!" ones.
+
+
+Update 07/29/2023:
+- Merged the two functions into one to reduce unnecessary complexity;
+- Changed comments and variable/function names to adhere to the "Clean Code" standards;
+- Added check_user_input function to check if an integer was typed;
+- Added line skips for clarity;
+- Rebuilt the executable with the aforementioned changes.
--- hello_triangular_recursive.py
+++ hello_triangular_recursive.py
@@ -1,31 +1,36 @@
-class hello:
-    
-    def hello_triangular_cres(self, num, a=1):  #recursive function that generates 'Hellos' from 1 to num#
-                                                ##a is an auxiliary variable used to get the inverse result from hello_triangular_decres##
-        Hello_World_end = "Hello!\n"            
-        Hello_World = "Hello! "    
+class HelloTriangular:
 
-        if num <= 0 or num - a <= 0:
-            return ""
-        else:
-            return (a-1)*(Hello_World) + Hello_World_end + self.hello_triangular_cres(num, a+1)
+    # Recursive function that generates 'Hellos' from 1 to num if direction == "crescent" and backwards from num to 1 if == "decrescent"
+    def generate_hello_triangular(self, num, direction, a=0):
+        
+        Hello_end_string = "Hello!\n"            
+        Hello_string = "Hello! "
+        
+        if direction == "crescent":                              
+            if num <= 0 or num - a <= 0:
+                return ""
+            else:
+                return (a)*(Hello_string) + Hello_end_string + self.generate_hello_triangular(num, direction, a+1)
+        
+        if direction == "decrescent":
+            if num <= 0:
+                return ""
+            else:
+                return (num-1)*(Hello_string) + Hello_end_string + self.generate_hello_triangular(num-1, direction)
+    def check_user_input(self):
+        while True:
+            try:
+                num = int(input("How many times do you want to say hello to everyone (triangularly)?\n"))       
+            except ValueError:
+                print("\nThis is not an integer.\n")
+                continue
+            else:
+                return num 
 
-    def hello_triangular_decres(self, num):     #recursive function that generates 'Hellos' from num-1 to 1#
-
-        Hello_World_end = "Hello!\n"
-        Hello_World = "Hello! "    
-
-        if num <= 0:
-            return ""
-        else:
-            return (num-1)*(Hello_World) + Hello_World_end + self.hello_triangular_decres(num-1)
-
-
+# Program run with user input#
 if __name__ == "__main__":                      
     num = 1
-    h = hello()
+    h = HelloTriangular()
     while num > 0:
-        num = int(input("How many times do you want to say hello to everyone (triangularly)? \n"))
-        print(h.hello_triangular_cres(num)+h.hello_triangular_decres(num))
-    
-    
+        num = h.check_user_input()
+        print("\n", h.generate_hello_triangular(num, "crescent")+h.generate_hello_triangular(num-1, "decrescent"), sep="")
